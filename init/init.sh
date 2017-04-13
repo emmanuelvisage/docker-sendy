45810d818886a07ac09c5d68691681dcb8346ad6
Emmanuel Marboeuf <emmanuel@visage.ae>
2017-04-12 18:46:47 -0700

    replaced host by image name

--- init/init.sh
+++ init/init.sh
@@ -3,8 +3,8 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-mysql --host=mysql-sendy --port=$MYSQL_PORT --user=root --password=$MYSQL_ROOT_PASSWORD -e "DROP DATABASE ${MYSQL_DATABASE};"
-mysql --host=mysql-sendy --port=$MYSQL_PORT --user=root --password=$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE ${MYSQL_DATABASE};"
-mysql --host=mysql-sendy --port=$MYSQL_PORT --user=root --password=$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < ./data/sendy_table_structure.sql
-mysql --host=mysql-sendy --port=$MYSQL_PORT --user=root --password=$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < ./data/sendy_base_data.sql
-mysql --host=mysql-sendy --port=$MYSQL_PORT --user=root --password=$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < ./data/sendy_users_$SENDY_ENV.sql
+mysql --host=mysql --port=$MYSQL_PORT --user=root --password=$MYSQL_ROOT_PASSWORD -e "DROP DATABASE ${MYSQL_DATABASE};"
+mysql --host=mysql --port=$MYSQL_PORT --user=root --password=$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE ${MYSQL_DATABASE};"
+mysql --host=mysql --port=$MYSQL_PORT --user=root --password=$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < ./data/sendy_table_structure.sql
+mysql --host=mysql --port=$MYSQL_PORT --user=root --password=$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < ./data/sendy_base_data.sql
+mysql --host=mysql --port=$MYSQL_PORT --user=root --password=$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < ./data/sendy_users_$SENDY_ENV.sql
